d138a0e4a9db45256be4a769395dd443bf780a7f
Shreyansh Agarwal <agarwal.shreyansh1313@gmail.com>
2018-04-26 16:31:48 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import {echo} from './actions/echo'
+import {serverMessage} from './reducers'
+
+class App extends Component {
+  componentDidMount() {
+      this.props.fetchMessage('Hi!')
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to POS Screen</h2>
+        <p>
+          {this.props.message}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ message: serverMessage(state) }),
+  { fetchMessage: echo }
+)(App);
